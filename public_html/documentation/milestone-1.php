ff23aba77eecb447b0bab6eba2cd0b9724ea602e
Jennifer Hung <jennifersukay505@gmail.com>
2015-11-03 10:19:16 -0700

    updated milestone-1.php

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -7,10 +7,10 @@
 		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
 		<!-- Bootstrap -->
-		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet"
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"
 				integrity="sha256-MfvZlkHCEqatNoGiOXveE8FIwMzZg4W85qfrfIFBfYc=
-		sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ=="
-				crossorigin="anonymous">
+sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ=="
+crossorigin="anonymous">
 		<link rel="stylesheet" href= "../../css/styles.css"/>
 
 		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
@@ -21,72 +21,40 @@
 		<![endif]-->
 		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.min.js"></script>
 		<script type="text/javacript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"
-				  integrity="sha256-Sk3nkD6mLTMOF0EOpNtsIry+s1CsaqQC1rVLTAy+0yc=
-					sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ=="
-				  crossorigin="anonymous"></script>
+integrity="sha256-Sk3nkD6mLTMOF0EOpNtsIry+s1CsaqQC1rVLTAy+0yc=sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/
+pQ==" crossorigin="anonymous"></script>
 
-			<head>
+	</head>
 	<body>
 		<header class="bgimage">
-			<div  class="container">
-				<h1>CodeGirl</h1>
+			<div  class="header">
+				<h1>Koi Koi</h1>
 			</div>
 		</header>
-		<div class="clearfix"></div>
-		<nav class="navbar navbar-inverse">
-			<!-- logo and mobile toggle button get grouped together for better mobile display-->
-			<div class="navbar-header">
-				<!-- this is the mobile menu button-->
-				<button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#main-menu">
-					<span class="sr-only">main menu</span>
-					<span class="glyphicon glyphicon-menu-hamburger"></span>
-				</button>
-				<div class="clearfix"></div>
-			</div>
-			<!-- here are your main nav links, grouped for toggling-->
-			<div class="collapse navbar-collapse" id="main-menu">
-				<ul class="nav navbar-nav navbar-right">
-					<li><a href="../../public_html/documentation/milestone-1.php">Home</a></li>
-					<li><a href="../../new.php">Links</a></li>
-					<li><a href="../../links/contact.php">Contact</a></li>
-				</ul>
-			</div>
-		</nav>
-
-	</body>
-</html>
-
-
-
-					<!-- improper floats
-					<div class="container">
-						<div class="row">
-						<div class="col-md-12"
-						<p id="sub">Coding Student at <a href="//bootcamp-coders.cnm.edu/">Deep Dive Coding Bootcamp</a></p>
-					</div>
+	<div class="clearfix"></div>
+	<div class="container" id="objective">
+		  <div class="row">
+			  <div class="col-md-12">
+					<dl>
+						<dt><span class="lead">Purpose</dt>
+						<dd>Offer light website design services way to contact me.<br></dd>
+						<dt><span class="lead">Audience</span></dt>
+						<dd>Entrepreneurs looking to have their website designed.</dd>
+						<dt><span class="lead">Goals</span></dt>
+						<dd>Attract potential freelance clients.</dd>
+					</dl>
 				</div>
-				</div>
-
-				<div class="container">
-				<div class="row">
-					<div class="col-md-6. col-md-offset-3">
-						<a href="../../links/contact.php"><img src="../../img/goalt.png" alt="goal"</a>
-						<a href="../../links/contact.php" title="email Jennifer!">Contact Designer</a>
-					</div>
-				</div>
-				</div>
-			</article>
-		</main>
-
-		<br>
-		<div class="footer">
-			<div class="leftCol"><p>all content @copy; copyright 2015 CodeGirlDesigns</p>
-				<p><a href="tel://505-489-9573" title="phone"</a></p>
-			</div>
+	 		</div>
 		</div>
-		-->
+
+
+
+
+
+
+
 		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 		<!-- Include all compiled plugins (below), or include individual files as needed -->
-		<script src="js/bootstrap.min.js"></script>
+		<script src=//js/bootstrap.min.js"></script>
 
