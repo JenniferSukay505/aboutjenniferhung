0b8865ac5db95649ddc45e42c3d33223c4d3d0fa
Jennifer Hung <jennifersukay505@gmail.com>
2015-11-11 16:14:37 -0700

    updated milestone-2.php

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -1,9 +1,128 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
+		<meta charset="utf-8"/>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet"/>
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" rel="stylesheet"/>
+		<script type="text/javascript" src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script type="text/javascript" src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/js-cookie/2.0.2/js.cookie.min.js"></script>
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.14.0/jquery.validate.min.js"></script>
+		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.14.0/additional-methods.min.js"></script>
+		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 		<title>milestone-2</title>
 	</head>
 	<body>
-		<p>Persona</p>
+		<article>
+			<header>
+				<h1>Persona and Use Case</h1>
+			</header>
+			<main>
+				<article>
+					<table class="table">
+						<caption>Persona</caption>
+						<thead>
+						<tbody>
+							<tr>
+								<th>Name</th>
+								<td>Christine Trielo</td>
+							</tr>
+							<tr>
+								<th>Age</th>
+								<td>28-years-old</td>
+							</tr>
+							<tr>
+								<th>Location</th>
+								<td>Albuquerque, NM</td>
+							</tr>
+							<tr>
+								<th>Profession</th>
+								<td>Restaurant Owner</td>
+							</tr>
+							<tr>
+								<th>Technology</th>
+								<td>Uses  a mac laptop 15-inch 2014 high retina and iphone 6. </td>
+							</tr>
+							<tr>
+								<th>Background</th>
+								<td> Opening a restaurant in Albuquerque and needs a website for advertisement. </td>
+								<td> Christine has poked into learning html for coding websites before,
+								but never had the time to learn.  However, she has learned enough to know what organized code looks like.  </td>
+							</tr>
+
+							<tr>
+								<th>Discovery</th>
+								<td>Found my site  while looking for local web designers while online</td>
+							</tr>
+							<tr>
+								<th>Attitudes and Behaviors</th>
+								<td>Calm but without patience to sloppiness. Christine wants things as quickly as possible with
+									attention to details.  Christine know the value of a business website but has no time
+									or patience to do one as quickly and perfectly as she wants herself. Thrifty and smart, Christine
+									knows the benefits one can gain and cost saved from hiring a newcomer, so she is not opposed to
+									smaller portfolios. However, she still requires a strong skillset for the new hire. </td>
+							</tr>
+							<tr>
+								<th>Frustrations and Needs</th>
+								<td>Christine is  perusing the web to hire a designer.  She needs a skilled developer that communicates
+									and adapts well under pressure.  The designer must also be able to perform and work within time constraints.
+									Christine has had people quit before under her micro-managing.
+							</tr>
+							<tr>
+								<th>Goals</th>
+								<td> Christine wants to reach her decison within a week of
+									comparing web developer sites.  Christine hopes to find a developer who is talented,
+									organized, and responsive to constructive criticism.  Christine wants to have her website
+									polished, up and running within three weeks of hiring the new developer.  The  hiree must
+									also be willing to work for minimum wage..
+					</table>
+					<section>
+						<p>Use Case</p>
+						<tr>
+							<th>
+								<td>Christine found my site while checking out freelancer.com, noted the recently published date, and
+								decided to take a closer look.  </td>
+							<td>After clicking though, though the site was sparce, she noted the site was easy to navigate through and
+							clearly laid out.  </td>
+							<td>After reading the About Me section and noting my education, Christine searched my name on google which pulled up my linkedIn and facebook profiles.
+								Both showed I was networked with groups and other developers and had a few comments on coding articles. We had the coding group "WomeninCoding" in common. </td>
+							<td>Christine did not like that a phone number wasn't listed, but decided to give me a chance and  sent an email from the contact link on my webpage to call her.</td>
+						</tr>
+					</section>
+
+					<section>
+						<h2>Proposed Site Map</h2>
+						<p> Site will consist of 3 pages with no sub-pages or sub-navigation.</p>
+						<ul>
+							<li>Home Page</li>
+							<li>Portfolio</li>
+							<li>Contact</li>
+						</ul>
+						<tr>
+							<th>Home Page</th>
+							<td>A brief introduction stating my name, education, and experience in coding.</td>
+							<td>This will include links to my social media profiles LinkedIn and Twitter.  </td>
+						</tr>
+
+						<tr>
+							<th>Portfolio</th>
+							<td>Examples of my work.  This will include links to websites as well as a description of my contributions
+							to the projects. </td>
+						</tr>
+						<tr>
+							<th>Contact</th>
+							<td>This page will contain a simple contact form to email me, my email address</td>
+						</tr>
+
+					</section>
+				</article>
+			</main>
 	</body>
 </html>
+
+
